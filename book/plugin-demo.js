87ac2a7f1aebff6f25381eac0ea58e701493c295
sun-help <923037840@qq>
2020-04-16 11:39:33 +0800

    modified:   book/plugin-demo.css 	modified:   book/plugin-demo.js         modified:   package.json 	modified:   package.json

--- book/plugin-demo.js
+++ book/plugin-demo.js
@@ -1,5 +1,19 @@
 var gitbook = window.gitbook;
-require(['gitbook','jquery'],function(gitbook,$)){
+
+gitbook.events.on('page.change', function() {
+	
+	var back_to_top_button = ['<div class="back-to-top"><i class="fa fa-arrow-up"></i></div>'].join("");
+	$(".book").append(back_to_top_button)
+
+	$(".back-to-top").hide();
+
+	$('.book-body,.body-inner').on('scroll', function () {
+		if ($(this).scrollTop() > 100) { 
+			$('.back-to-top').fadeIn();
+		} else {
+			$('.back-to-top').fadeOut();
+		}
+	});
 	var fetch = require("node-fetch");
     var fs = require("fs");
 	function download(){
@@ -11,12 +25,13 @@ require(['gitbook','jquery'],function(gitbook,$)){
 			}
 		})
 	}
-	gitbook.events.on('page.change',function(){
-		var back_to_top_button = ['<div class="sunhao"><i class="demo"></i></div>'].join("");
-	    	$(".book").append(back_to_top_button);
-		$('.sunhao').click(function(){
-			download();
-		})
-		});
+
+	$('.back-to-top').click(function () { 
+		download();
+		$('.book-body,.body-inner').animate({
+			scrollTop: 0
+		}, 800);
+		return false;
+	});
 	
 });
